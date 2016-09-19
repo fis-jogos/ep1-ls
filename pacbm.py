8cd8a91302e516e95c3a59eec67cacdb38992683
Lucas Souza <lucas.soaresouza@gmail.com>
2016-09-19 01:58:52 -0300

    Criada a classe do personagem principal 'pacbm.py'

---
+++ pacbm.py
@@ -0,0 +1,11 @@
+class pacbumman:
+	#atributos
+	def __init__ (self, firstimage, secondimage, status, posx, posy)
+	self.firstimage
+	self.secondimage
+	self.status
+	self.posx
+	self.posy
+
+
+
---
+++ pacbumman.py
@@ -0,0 +1,12 @@
+from pacbumman import *
+
+#Dimensões da tela
+WIDTH = 1000
+HEIGHT = 600
+
+#Constantes
+
+#Criar PacBUm
+
+
+
