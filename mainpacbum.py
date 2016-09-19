f1c978f517f80c5cce1366d5375d86881220cd69
Lucas Souza <lucas.soaresouza@gmail.com>
2016-09-19 11:11:21 -0300

    Controle da gravidade removido, controle das forcas x e y adicionados

--- mainpacbum.py
+++ mainpacbum.py
@@ -10,78 +10,84 @@ HEIGHT = 700
 #Ambientação
 env = environment(gravity = 10)
 
-PacBum = PacBum(Actor('persona_1'), mass = 1000, gravity  = env.gravity, status = True, imagestatus = False, 
-				posx =  110, posy = 580, velx = 400, vely = -0, acx = 0, acy = 0, forcex =0, forcey = 0)
+PacBum = PacBum(Actor('persona_1'), mass = 500, gravity  = env.gravity, status = True, imagestatus = False, 
+				posx =  110, posy = 580, velx = 0, vely = -0, acx = 0, acy = 0, forcex =0, forcey = 0)
 
 Cannon = Cannon(Actor('canhao'), mass = 1, gravity = env.gravity, posx = 100, posy = 590, theta = 0)
 
 WheelCannon = Actor('roda_canhao', pos=(30, 630))
 
-TnumberGravity = Actor('0', pos=(600,325))
-UnumberGravity = Actor('0', pos=(680,325))
+Tnumber = Actor('0', pos=(530,325))
+Unumber = Actor('0', pos=(630,325))
+Vnumber = Actor('0', pos=(730, 325))
 BoardView = Actor('gravidade', pos=(630, 200))
 
 
 #Objetos no ambiente
 env.add(PacBum)
-env.add(Cannon)
+#env.add(Cannon)
 env.add(WheelCannon)
 
 
 def draw():
 	env.draw()
 
-
+def convertnumber(value):
+	number = value / 100
+	number = int(number)
+	number = str(number)
+	return number
 
 def update(dt):
 
 	screen.clear()
 
+	#Mostrar Força X
+	if keyboard.q:
+		BoardView.image = 'forca'
+		aux = convertnumber(PacBum.forcex)
+		Tnumber.image = aux
+		BoardView.draw()
+		Tnumber.draw()
+		Unumber.draw()
+		Vnumber.draw()
+		time.delay(1000)
+
+	#Mostrar Força Y
+	if keyboard.w:
+		BoardView.image = 'forca'
+		aux = convertnumber(PacBum.forcey)
+		Tnumber.image = aux
+		BoardView.draw()
+		Tnumber.draw()
+		Unumber.draw()
+		Vnumber.draw()
+		time.delay(1000)
+
+	#Add Força X
+	if keyboard.f & keyboard.p:
+		PacBum.addforcex()
+		time.delay(500)
+
+	#Remove Força X
+	if keyboard.f & keyboard.l:
+		PacBum.rmforcex()
+		time.delay(500)
+
+
+	#Add Força Y
+	if keyboard.r & keyboard.p:
+		PacBum.addforcey()
+		time.delay(500)
+
+	#Remove Força Y
+	if keyboard.r & keyboard.l:
+		PacBum.rmforcey()
+		time.delay(500)
+
 	if keyboard.space:
 		PacBum.acelerationXY()
 		PacBum.velocityXY(dt)
 		PacBum.positionXY(dt)
-		if(PacBum.posy > 665):
-			PacBum.gravity = 0
-			PacBum.velx = 0
-			PacBum.vely = 0
-
-	if keyboard.g:
-
-		if env.gravity < 100:
-			TnumberGravity.image = '0'
-			UnumberGravity.draw()
-			TnumberGravity.draw()
-			BoardView.draw()
-		else:
-			TnumberGravity.image = Tnumber
-			BoardView.image = 'gravidade'
-			UnumberGravity.draw()
-			TnumberGravity.draw()
-			BoardView.draw()
 
 
-	if keyboard.g & keyboard.up:
-
-		if env.gravity > 900:
-			env.gravity = 900
-			TnumberGravity.image = '9'
-		else:
-			env.gravity += 100
-			time.delay(400)
-			Tnumber = env.gravity / 100
-			Tnumber = int(Tnumber)
-			Tnumber = str(Tnumber)
-
-
-	if keyboard.g & keyboard.down:
-
-		if env.gravity < 100:
-			env.gravity = 100
-			TnumberGravity.image = '1'
-		else:
-			env.gravity -= 100
-			time.delay(400)
-			Tnumber = env.gravity / 100
-			Tnumber = int(Tnumber)
-			Tnumber = str(Tnumber)
